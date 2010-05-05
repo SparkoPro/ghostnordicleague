889e5ad6b41294482bdfb4062ebbaafd017283e6
fredrik.sigillet <fredrik.sigillet@4a4c9648-eef2-11de-9456-cf00f3bddd4e>
2010-05-05 04:48:15 +0000

    fixed a bug with autostart off and score checks

--- ghost/game_base.cpp
+++ ghost/game_base.cpp
@@ -1652,7 +1652,8 @@ void CBaseGame :: EventPlayerJoined( CPotentialPlayer *potential, CIncomingJoinP
 		}
 	}
 
-	if( m_MatchMaking && m_AutoStartPlayers != 0 && !m_Map->GetMapMatchMakingCategory( ).empty( ) && m_Map->GetMapGameType( ) == GAMETYPE_CUSTOM )
+//	if( m_MatchMaking && m_AutoStartPlayers != 0 && !m_Map->GetMapMatchMakingCategory( ).empty( ) && m_Map->GetMapGameType( ) == GAMETYPE_CUSTOM )
+	if( m_MatchMaking && !m_Map->GetMapMatchMakingCategory( ).empty( ) && m_Map->GetMapGameType( ) == GAMETYPE_CUSTOM )
 	{
 		// matchmaking is enabled
 		// start a database query to determine the player's score
@@ -2407,11 +2408,6 @@ potential->GetExternalIPString( ) + "] is trying to join the game but has a rati
 		}
 	}
 
-	if( score < -99999.0 )
-		SendAllChat( m_GHost->m_Language->PlayerHasScore( joinPlayer->GetName( ), "N/A" ) );
-	else
-		SendAllChat( m_GHost->m_Language->PlayerHasScore( joinPlayer->GetName( ), UTIL_ToString( score, 2 ) ) );
-
 	uint32_t PlayersScored = 0;
 	uint32_t PlayersNotScored = 0;
 	double AverageScore = 0.0;
@@ -2442,7 +2438,15 @@ potential->GetExternalIPString( ) + "] is trying to join the game but has a rati
 	}
 
 	double Spread = MaxScore - MinScore;
-	SendAllChat( m_GHost->m_Language->RatedPlayersSpread( UTIL_ToString( PlayersScored ), UTIL_ToString( PlayersScored + PlayersNotScored ), UTIL_ToString( (uint32_t)Spread ) ) );
+	
+	string PlayerHasScore;
+	
+	if( score < -99999.0 )
+		PlayerHasScore = m_GHost->m_Language->PlayerHasScore( joinPlayer->GetName( ), "N/A" );
+	else
+		PlayerHasScore = m_GHost->m_Language->PlayerHasScore( joinPlayer->GetName( ), UTIL_ToString( score, 2 ) );
+		
+	SendAllChat( PlayerHasScore + " " + m_GHost->m_Language->RatedPlayersSpread( UTIL_ToString( PlayersScored ), UTIL_ToString( PlayersScored + PlayersNotScored ), UTIL_ToString( (uint32_t)Spread ) ) );
 
 	// check for multiple IP usage
 
